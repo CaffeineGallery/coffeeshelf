ba59dc14851e24dc3c6904e34c919b99b96014e4
Leon <132808925+Leon8326@users.noreply.github.com>
2025-05-17 21:09:20 +0200

    Update shortcuts.sh

--- modules/shortcuts.sh
+++ modules/shortcuts.sh
@@ -2,8 +2,9 @@
 
 shortcuts_panel() {
   echo -e "\e[1;36m📝 Shortcuts:\e[0m"
-  echo "[1] Open File Manager:    dolphin ."
-  echo "[2] Edit Config:          nano ~/.caffeine/config"
-  echo "[3] Update Coffee:        coffee update"
-  echo "[4] Reload Shelf:         caffeine shelf"
+  echo "[1] Open File Manager:      dolphin ."
+  echo "[2] Edit Config:            nano ~/.caffeine/config"
+  echo "[3] Update your packages:   sudo bitey update"
+  echo "[4] Reload Shelf:           shelf"
+  echo "[5] Exit Shelf:             exit"
 }
