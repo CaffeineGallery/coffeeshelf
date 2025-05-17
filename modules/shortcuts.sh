2a6d301fd17a22287e6932def97a71b9aeea0505
Leon <132808925+Leon8326@users.noreply.github.com>
2025-05-17 19:50:52 +0200

    Create shortcuts.sh

---
+++ modules/shortcuts.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+shortcuts_panel() {
+  echo -e "\e[1;36m📝 Shortcuts:\e[0m"
+  echo "[1] Open File Manager:    dolphin ."
+  echo "[2] Edit Config:          nano ~/.caffeine/config"
+  echo "[3] Update Coffee:        coffee update"
+  echo "[4] Reload Shelf:         caffeine shelf"
+}
