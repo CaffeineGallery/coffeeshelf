5f977a073db5fde1fb6f01d4d1d0469efdc19a18
Leon <132808925+Leon8326@users.noreply.github.com>
2025-05-18 11:45:15 +0200

    Update shortcuts.sh

--- modules/shortcuts.sh
+++ modules/shortcuts.sh
@@ -4,6 +4,4 @@ shortcuts_panel() {
   echo -e "📝 Shortcuts:"
   echo "[1] Open File Manager:      dolphin ."
   echo "[2] Update your packages:   sudo bitey update"
-  echo "[3] Reload Shelf:           refresh"
-  echo "[4] Exit Shelf:             exit"
 }
